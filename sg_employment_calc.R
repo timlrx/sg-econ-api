da395d0acb7b40c5a819546f81aeec97ab5708f5
Lenovo\timothy <timothy0336@hotmail.com>
2018-02-18 17:30:56 +0800

    corrected date ordering

--- sg_employment_calc.R
+++ sg_employment_calc.R
@@ -6,13 +6,13 @@ files_gdp <- list.files("data/gdp/")
 files_prd <- list.files("data/productivity/")
 
 date_list_gdp <- gsub("(.+)(-gdp.RData)","\\1",files_gdp)
-recent_date_gdp <- date_list_gdp[order(format(as.Date(date_list_gdp),"%m%d"))[length(date_list_gdp)]]
+recent_date_gdp <- date_list_gdp[order(format(as.Date(date_list_gdp),"%Y-%m-%d"))[length(date_list_gdp)]]
 load(paste("data/gdp/",recent_date_gdp,"-gdp.RData", sep=""))
 gdp <- va.sector.comb %>%
        select(industry, period, va)
 
 date_list_prd <- gsub("(.+)(-productivity.RData)","\\1",files_prd)
-recent_date_prd <- date_list_prd[order(format(as.Date(date_list_prd),"%m%d"))[length(date_list_prd)]]
+recent_date_prd <- date_list_prd[order(format(as.Date(date_list_prd),"%Y-%m-%d"))[length(date_list_prd)]]
 load(paste("data/productivity/",recent_date_prd,"-productivity.RData", sep=""))
 productivity <- productivity %>%
                 select(industry, period, industry.short, year, quarter, productivity)
